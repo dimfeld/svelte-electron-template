fff481ad465dc0add47761b7999f694b24dedb51
Daniel Imfeld <daniel@danielimfeld.com>
2020-04-20 08:01:52 +0000

    adapt to electron build

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -5,7 +6,6 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
-import html, { makeHtmlAttributes } from '@rollup/plugin-html';
 import copy from 'rollup-plugin-copy';
 
 const production = process.env.NODE_ENV !== 'development';
@@ -14,7 +14,7 @@ const babelConfig = {
   extensions: ['.js', '.mjs', '.html', '.svelte', '.ts'],
   exclude: ['node_modules/@babel/**', 'static/**', 'build/**', 'public/**'],
   presets: [
-    ['@babel/preset-env', { targets: { esmodules: true } }],
+    ['@babel/preset-env', { targets: { chrome: 80 } }],
     '@babel/preset-typescript',
   ],
   plugins: [
@@ -29,12 +29,10 @@ const babelConfig = {
 export default {
   input: 'src/main.js',
   output: {
-    dir: 'build',
-    entryFileNames: '[name].[hash].js',
-    chunkFileNames: '[name].[hash].js',
-    assetFileNames: '[name].[hash][extname]',
-    sourcemap: production ? true : 'inline',
-    format: 'esm',
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'build/bundle.js',
   },
   plugins: [
     svelte({
@@ -43,14 +41,12 @@ export default {
       dev: !production,
       // extract any component CSS out into
       // a separate file — better for performance
-      // css: css => {
-      //     css.write('public/bundle.css');
-      // },
-      // Emit CSS as a file for processing through rollup
-      emitCss: true,
+      css: (css) => {
+        css.write('build/app.css');
+      },
     }),
     postcss({
-      extract: true,
+      extract: 'util.css',
     }),
 
     resolve({
@@ -63,7 +59,6 @@ export default {
       targets: [{ src: 'static/**/*', dest: 'build/' }],
     }),
     commonjs(),
-    json(),
     babel(babelConfig),
 
     // Watch the `build` directory and refresh the
@@ -73,37 +68,6 @@ export default {
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser(),
-
-    html({
-      title: 'A Svelte Website',
-      template: ({ attributes, files, publicPath, title }) => {
-        let templateFile = fs.readFileSync('src/index.html');
-
-        // This is adapted from the default template function in the HTML plugin.
-        const scripts = (files.js || [])
-          .map(({ fileName }) => {
-            const attrs = makeHtmlAttributes(attributes.script);
-            return `<script src="${publicPath}${fileName}"${attrs}></script>`;
-          })
-          .join('\n');
-
-        const links = (files.css || [])
-          .map(({ fileName }) => {
-            const attrs = makeHtmlAttributes(attributes.link);
-            return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
-          })
-          .join('\n');
-
-        let exec = _.template(templateFile.toString());
-        return exec({
-          attributes,
-          title,
-          scripts,
-          links,
-          htmlAttributes: makeHtmlAttributes(attributes.html),
-        });
-      },
-    }),
   ],
   watch: {
     clearScreen: false,
